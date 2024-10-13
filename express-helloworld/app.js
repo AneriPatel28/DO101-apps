29146facb648755478e80848b18be5b60250e9c5
AneriPatel28 <aneri2220@gmail.com>
2024-10-13 19:50:21 -0400

    v2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -6,13 +6,6 @@ app.get('/', function (req, res) {
 });
 var response;
 
-app.get('/', function (req, res) {
-    response = 'This is version 2 of the app.' + '\n';
-
-    //send the response to the client
-    res.send(response);
-
-});
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
